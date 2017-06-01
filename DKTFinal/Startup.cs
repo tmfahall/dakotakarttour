f6c5d396ee8cc8281ff755efffe87953a17d0f63
tmfahall <tmfahall@gmail.com>
2017-05-31 21:50:14 -0500

    Add project files.

---
+++ DKTFinal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DKTFinal.Startup))]
+namespace DKTFinal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
